6eb6340e0ca36cbdc23214bfd6652df0ac243fc8
AyushChandraJha <ayushchandra2022@outlook.com>
2024-11-08 15:06:00 +0000

    new commit

--- 01_Basics/02_dataTypes.js
+++ 01_Basics/02_dataTypes.js
@@ -2,8 +2,8 @@
 
 // alert(3+3) we are using nodeJS not browser
 
-console.log(3+3); // code readablity should be high...
-console.log("Ayush");
+//console.log(3+3); // code readablity should be high...
+//console.log("Ayush");
 
 let name = "Ayush";
 let age = 35;
@@ -13,4 +13,22 @@ let isLoggedIn = true;
 // symbol => unique
 
 /// object
-console.log(typeof null);
+//console.log(typeof null);
+
+// 1. Primitive Data Types : 7 types => String, Boolean, null, undefined, symbol, bigInt
+// 2. Reference (Non-Primitive): Array, Objects , Functions
+
+let heros = ["Sahaktimaan", "Nagraj","Doga","Dhruv"];
+let myObject = { name: "Ayush", age: 36};
+let myFunction = function(){
+    console.log("Hello World");
+    
+}
+console.log(typeof heros);
+console.log(typeof myObject);
+console.log(typeof myFunction);
+
+
+// ***********************************************************************
+// Stack Memory (Primitive) =>
+// Heap Memory (Non-Primitive) =>
---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+// let score = "36";
+// console.log(typeof score);
+
+// let valueInNumber = Number(score); // conversion to number
+// console.log(typeof valueInNumber);
+
+// let num1 = 15;
+// let numToString = String(num1);
+// console.log(typeof numToString);
+
+//**************************************Operations*******************************************  */
+
+// 
+let myName = "Ayush Chandra";
+let myNewName = myName;
+//console.log(myNewName);
+myNewName = "mr Jha";
+//console.log(myNewName);
+//console.log(myName);
+
+let user1 = {
+    name: "abcd", upi: "400118@ybl"
+}
+
+let user2 = user1;
+user2.upi = "427096@rbl";
+console.log(user1);
+console.log(user2);
+
+
+
+
